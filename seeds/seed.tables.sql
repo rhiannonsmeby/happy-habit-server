d1fa3a8f5a4030b7df029e8594670b816b438f87
Rhiannon Smeby <smebyrhi@gmail.com>
2021-03-09 18:46:02 -0600

    updates to tables and seeds

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,18 +1,18 @@
 BEGIN;
 
 TRUNCATE
-    user,
-    activity,
-    mood_log
+    users,
+    activities,
+    mood_logs
     RESTART IDENTITY CASCADE;
 
-INSERT INTO user (name, username, email, password) 
+INSERT INTO users (full_name, username, email, password) 
 VALUES
 ('Demo User', 'demo_user', 'demo@gmail.com', 'password1'),
 ('Harrison Baty', 'harrisonb', 'harris@gmail.com', 'password2'),
 ('Sarah', 'thebirdspapaya', 'thebirdspapaya@biz.com', 'password3');
 
-INSERT INTO activity (assigned_user, activity_name)
+INSERT INTO activities (assigned_user, activity_name)
 VALUES
 (1, 'Journal'),
 (1, 'Go for a walk'),
@@ -21,10 +21,11 @@ VALUES
 (3, 'Watch an episode of my favorite TV show'),
 (3, 'Take a shower');
 
-INSERT INTO mood_log (assigned_user, assigned_activity, start_mood, end_mood, notes)
+INSERT INTO mood_logs (assigned_user, assigned_activity, start_mood, end_mood, notes)
+VALUES
 (1, 1, 1, 3, 'Today I used a journal propmt about gratitude. I finished about 20 minutes of journaling feeling much better.'),
 (2, 4, 1, 2, 'Not the major mood boost I was looking for'),
-(3, 5, 2, 4),
+(3, 5, 2, 4, ''),
 (3, 6, 2, 5, 'Sometimes when I think I am sad, I am really just dirty haha');
 
 COMMIT;
